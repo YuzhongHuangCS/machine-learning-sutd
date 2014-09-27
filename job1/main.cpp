03b17200cc87fe3f535ce558f0517b2b6922257d
Yuzo <hyzgog@gmail.com>
2014-09-27 22:05:59 +0800

    use pair

--- job1/main.cpp
+++ job1/main.cpp
@@ -4,31 +4,30 @@
 
 using namespace std;
 
-// function defines
+// type deinfe
 typedef array<double, 3> Row;
 typedef array<double, 3> Para;
+typedef pair<int, Para> Score;
 
+// function defines
 list<Row> readFile(string fileName);
 int sign(const Row &row, const Para &para);
 int errors(const Para &para);
 int predictErrors(const Para &para);
 Para patch(const Para &para, const Row &row);
 list<Para> analysys(const Para &para);
-array<double, 4> deepin(int i, Para para, int depth);
+Score deepin(int i, Para para, int depth);
 
 // preset data defines
 Para initPara = {0, 0, 0};
 auto trainFileContent = readFile("train_1_5.csv");
 auto testFileContent = readFile("test_1_5.csv");
 
+// main
 int main(void) {
 	auto finalResult = deepin(0, initPara, 1);
-	int bestErrors = finalResult[0];
-	Para bestPara = {
-		finalResult[1],
-		finalResult[2],
-		finalResult[3],
-	};
+	int bestErrors = finalResult.first;
+	Para bestPara = finalResult.second;
 	int predictionError = predictErrors(bestPara);
 
 	cout << bestErrors << endl;
@@ -36,6 +35,7 @@ int main(void) {
 	cout << predictionError << endl;
 }
 
+// function implementation
 list<Row> readFile(string fileName) {
 	ifstream inFile(fileName);
 	string line;
@@ -92,13 +92,13 @@ int predictErrors(const Para &para) {
 }
 
 Para patch(const Para &para, const Row &row) {
-	Para newPara = {
+	Para tryPara = {
 		para[0] + row[2] * row[0],
 		para[1] + row[2] * row[1],
 		para[2] + row[2]
 	};
 
-	return newPara;
+	return tryPara;
 }
 
 list<Para> analysys(const Para &para) {
@@ -114,25 +114,24 @@ list<Para> analysys(const Para &para) {
 	return candidatePara;
 }
 
-array<double, 4> deepin(int i, Para para, int depth) {
-	list< pair<int, Para> > thisResult;
-	
-	thisResult.push_back(pair<int, Para>(errors(para), para));
+Score deepin(int i, Para para, int depth) {
+	list<Score> thisResult;
+	thisResult.push_back(Score(errors(para), para));
 
 	if(i < depth){
 		auto tryParas = analysys(para);
 
 		for(auto it = tryParas.begin(); it != tryParas.end(); it++){
-			Para &subPara = *it;
-			auto subResult = deepin(i+1, subPara, depth);
-			thisResult.push_back( pair<int, Para>(subResult[0], {subResult[1], subResult[2], subResult[3]}));
+			Para &childPara = *it;
+			auto childResult = deepin(i+1, childPara, depth);
+			thisResult.push_back(Score(childResult.first, childResult.second));
 		}
 	} else{
 		auto tryParas = analysys(para);
 
 		for(auto it = tryParas.begin(); it != tryParas.end(); it++){
 			Para &subPara = *it;
-			thisResult.push_back( pair<int, Para>(errors(subPara), subPara));
+			thisResult.push_back(Score(errors(subPara), subPara));
 		}
 	}
 
@@ -146,11 +145,5 @@ array<double, 4> deepin(int i, Para para, int depth) {
 		}
 	}
 
-	array<double, 4> returnData = {
-		bestError,
-		bestPara[0],
-		bestPara[1],
-		bestPara[2]
-	};
-	return(returnData);
+	return(Score(bestError, bestPara));
 }
